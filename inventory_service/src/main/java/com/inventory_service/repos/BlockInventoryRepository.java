95ee56441e9be89f282236851d1deb929c28493c
Hiren Thacker <hithacker@gmail.com>
2023-06-22 12:40:58 +0530

    Fixing issue in BlockInventoryRepository

--- inventory_service/src/main/java/com/inventory_service/repos/BlockInventoryRepository.java
+++ inventory_service/src/main/java/com/inventory_service/repos/BlockInventoryRepository.java
@@ -1,7 +1,6 @@
 package com.inventory_service.repos;
 
 import com.inventory_service.entities.BlockInventory;
-import com.inventory_service.entities.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface BlockInventoryRepository extends JpaRepository<BlockInventory, UUID> {
-    @Query(value = "SELECT product_id, SUM(quantity) AS totalQuantity FROM block_inventories WHERE product_id IN :productIds AND expiry_time > :expiryTime GROUP BY product_id",nativeQuery = true)
+    @Query(value = "SELECT product_id, CAST(SUM(quantity) AS INT) AS totalQuantity FROM block_inventories WHERE product_id IN :productIds AND expiry_time > :expiryTime GROUP BY product_id",nativeQuery = true)
     List<Object[]> findTotalQuantityByProductIds(@Param("productIds") List<UUID> productIds, @Param("expiryTime") Instant expiryTime);
 
     @Modifying
